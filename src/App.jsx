e040e3d9de3572a422542cf5db00ea60f2b024cc
Lucas Lisboa <lucaslisboamotta@gmail.com>
2022-09-01 21:41:40 -0300

    feat: Implementando componente Form no APP

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import Form from './components/Form';
+import register from './api/registerExemptionRequests';
 import './App.css';
 
 function App() {
+  const registerFunction = async (payload) => {
+    await register(payload);
+  };
   return (
-    <div className="App" />
+    <div className="App">
+      <Form onClickRegisterButton={registerFunction} />
+    </div>
   );
 }
 
