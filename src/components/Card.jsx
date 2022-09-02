c7ad7d88c724a643bf9eea44eef24d81434e2b81
Lucas Lisboa <lucaslisboamotta@gmail.com>
2022-09-02 20:36:52 -0300

    feat: Alterando o formato de exibição da data e cpf

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,26 +2,41 @@ import React from 'react';
 import { shape, number, string } from 'prop-types';
 
 export default function Card({ request }) {
+  const refactorCPF = (cpf) => {
+    const newCPF = cpf.split('');
+    newCPF.splice(3, 0, '.');
+    newCPF.splice(7, 0, '.');
+    newCPF.splice(11, 0, '-');
+    return newCPF.join('');
+  };
+
+  const refactorDate = (date) => date.split('-').reverse().join('/');
+
   return (
     <tr>
       <td>{request.id}</td>
       <td>{request.name}</td>
       <td>{request.email}</td>
-      <td>{request.cpf}</td>
-      <td>{request.date}</td>
+      <td>{refactorCPF(request.cpf)}</td>
+      <td>{refactorDate(request.date)}</td>
       <td>{request.latitude_location}</td>
       <td>{request.longitude_location}</td>
-      <td>{request.event}</td>
-      <td>{request.create_at}</td>
+      <td>{request.event.toLowerCase()}</td>
+      <td>{refactorDate(request.create_at)}</td>
     </tr>
   );
 }
 
 Card.propTypes = {
   request: shape({
-    id: number,
+    id: string,
     name: string,
     email: string,
-    role: string,
+    cpf: string,
+    latitude_location: number,
+    longitude_location: number,
+    date: string,
+    event: string,
+    create_at: string,
   }).isRequired,
 };
