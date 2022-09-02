5d58fb9cb1eedb096df39bcb7a101d5cedb671a9
Lucas Lisboa <lucaslisboamotta@gmail.com>
2022-09-01 21:37:53 -0300

    fix: Corigindo chaves de entrada

--- src/utils/verifyInputs.js
+++ src/utils/verifyInputs.js
@@ -6,7 +6,7 @@ export const verifyEmail = (email) => /\S+@\S+\.\S+/.test(email);
 
 export const verifyCPF = (CPF) => /(^\d{11}$)/g.test(CPF);
 
-export const verifyDate = (date) => new Date(date) <= Date.now;
+export const verifyDate = (date) => new Date(date) <= Date.now();
 
 export const verifyLatitude = (latitude) => typeof latitude === 'number';
 
@@ -15,17 +15,18 @@ export const verifyLongitude = (longitude) => typeof longitude === 'number';
 export const verifyEvent = (event) => eventList.includes(event);
 
 export const verifyAll = ({
-  name, email, CPF, date, latitude, longitude, event,
+  name, email, cpf, date, latitude_location: latitude, longitude_location: longitude, event,
 }) => {
   const boolArrayVerify = [
     verifyName(name),
     verifyEmail(email),
-    verifyCPF(CPF),
+    verifyCPF(cpf),
     verifyDate(date),
     verifyLatitude(latitude),
     verifyLongitude(longitude),
     verifyEvent(event),
   ];
+  console.log(boolArrayVerify);
 
   return boolArrayVerify.every((bool) => bool);
 };
