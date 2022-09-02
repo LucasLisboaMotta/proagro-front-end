80943c7f8a009dbc80058b80411c678f72aff0ea
Lucas Lisboa <lucaslisboamotta@gmail.com>
2022-09-02 01:07:43 -0300

    feat: implementando component para listar pedidos Devido a alguns problemas, parte do commit sera com comentarios até os problemas serem resolvidos.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Form from './components/Form';
 import register from './api/registerExemptionRequests';
+// import ExemptionRequestList from './components/ExemptionRequestList';
+import getAllExemptionRequests from './api/apiConnection';
 import './App.css';
 
 function App() {
+  const [requestArray, setRequestArray] = useState([]);
+
+  const updateRequestArray = async () => {
+    const newRequestArray = await getAllExemptionRequests();
+    console.log(newRequestArray);
+    setRequestArray(newRequestArray);
+  };
+
   const registerFunction = async (payload) => {
     await register(payload);
+    updateRequestArray();
   };
+
+  useEffect(() => {
+    updateRequestArray();
+  }, []);
+
   return (
     <div className="App">
       <Form onClickRegisterButton={registerFunction} />
+      {/* <ExemptionRequestList requestArray={requestArray} /> */}
     </div>
   );
 }
