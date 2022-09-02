3b4cc8be0f19f531ee33ab2d25c614e5e67a1aa5
Lucas Lisboa <lucaslisboamotta@gmail.com>
2022-09-02 01:04:42 -0300

    refactor: Alerando nome do arquivo de conexão

---
+++ src/api/apiConnection.js
@@ -0,0 +1,9 @@
+// import 'dotenv/config';
+
+import axios from 'axios';
+
+const API = axios.create({
+  baseURL: 'http://localhost:8000',
+});
+
+export default API;
--- src/api/index.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-const axios = require('axios');
-
-const api = axios.create({
-  baseURL: process.env.API_URL,
-});
-
-module.exports = api;
