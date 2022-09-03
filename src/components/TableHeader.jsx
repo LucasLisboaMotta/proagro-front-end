d0b939eeee2d33f3f1baf97f5ceaac121eedbc70
Lucas Lisboa <lucaslisboamotta@gmail.com>
2022-09-03 17:02:10 -0300

    feat: Criando cabeçalho do tipo de lavoura

---
+++ src/components/TableHeader.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function TableHeader() {
+  return (
+    <thead>
+      <tr>
+        <th>Nome</th>
+        <th>E-mail</th>
+        <th>CPF</th>
+        <th>Tipo de lavoura</th>
+        <th>Data do evento</th>
+        <th>Latitude</th>
+        <th>Longitude</th>
+        <th>Tipo de evento</th>
+        <th>Data da requisição</th>
+        <th>Editar/Excluir</th>
+      </tr>
+    </thead>
+  );
+}
