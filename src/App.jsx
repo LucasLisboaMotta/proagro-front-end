f88a83d2998390e597554fa3633b2c8550f2bf92
Lucas Lisboa <lucaslisboamotta@gmail.com>
2022-09-03 17:01:15 -0300

    refactor: Removendo funções desnecessarias

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Form from './components/Form';
-import register from './api/registerExemptionRequests';
 import ExemptionRequestList from './components/ExemptionRequestList';
-import getAllExemptionRequests from './api/getAllExemptionRequests';
+import DivergentRequest from './components/DivergentRequest';
 import Provider from './context/provider';
 import './App.css';
 
 function App() {
-  const [requestArray, setRequestArray] = useState([]);
-
-  const updateRequestArray = async () => {
-    const newRequestArray = await getAllExemptionRequests();
-    setRequestArray(newRequestArray);
-  };
-
-  const registerFunction = async (payload) => {
-    await register(payload);
-    updateRequestArray();
-  };
-
-  useEffect(() => {
-    updateRequestArray();
-  }, []);
-
   return (
     <div className="App">
       <Provider>
-        <Form onClickRegisterButton={registerFunction} />
-        <ExemptionRequestList requestArray={requestArray} />
+        <Form />
+        <DivergentRequest />
+        <ExemptionRequestList />
       </Provider>
     </div>
   );
