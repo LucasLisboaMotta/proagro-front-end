f83286c2c8c9d5a4beaf7210ab1cb7bc89942f8f
Lucas Lisboa <lucaslisboamotta@gmail.com>
2022-09-03 17:06:12 -0300

    refactor: Componentizando cabeçalho de tabela

--- src/components/ExemptionRequestList.jsx
+++ src/components/ExemptionRequestList.jsx
@@ -1,35 +1,37 @@
 import React, { useContext } from 'react';
 import context from '../context/context';
+import TableHeader from './TableHeader';
 import Card from './Card';
 
 export default function ExemptionRequestList() {
   const { requestArray } = useContext(context);
-  const sortRequest = ({ create_at: createA }, { create_at: createB }) => {
+
+  const sortRequest = (
+    { create_at: createA, name: nameA },
+    { create_at: createB, name: nameB },
+  ) => {
     if (new Date(createA) < new Date(createB)) return 1;
     if (new Date(createA) > new Date(createB)) return -1;
+    if (nameA > nameB) return 1;
+    if (nameA < nameB) return -1;
     return 0;
   };
 
   const mapCards = (request) => <Card request={request} key={`card-list-${request.id}`} />;
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Nome</th>
-          <th>E-mail</th>
-          <th>CPF</th>
-          <th>Data do evento</th>
-          <th>Latitude</th>
-          <th>Longitude</th>
-          <th>Tipo de evento</th>
-          <th>Data da requisição</th>
-          <th>Editar/Excluir</th>
-        </tr>
-      </thead>
-      <tbody>
-        {requestArray.sort(sortRequest).map(mapCards)}
-      </tbody>
-    </table>
+    requestArray.length > 0
+      ? (
+        <div>
+          <h2>Requisições feitas</h2>
+          <table>
+            <TableHeader />
+            <tbody>
+              {requestArray.sort(sortRequest).map(mapCards)}
+            </tbody>
+          </table>
+        </div>
+      )
+      : <div><h2>Nenhuma requisição encontrada</h2></div>
   );
 }
