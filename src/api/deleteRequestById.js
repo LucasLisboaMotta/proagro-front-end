817a8f960025c6289d6160a4bc90ca8e15787abe
Lucas Lisboa <lucaslisboamotta@gmail.com>
2022-09-04 20:00:25 -0300

    refactor: retirando configuração por header

--- src/api/deleteRequestById.js
+++ src/api/deleteRequestById.js
@@ -1,7 +1,5 @@
 import API from './apiConnection';
 
-const headers = { headers: { 'Access-Control-Allow-Origin': 'http://localhost:8000', 'content-type': 'aplication/json' } };
-
-const deleteRequest = (id) => API.delete(`exemptionrequests/${id}`, headers);
+const deleteRequest = (id) => API.delete(`exemptionrequests/${id}`);
 
 export default deleteRequest;
