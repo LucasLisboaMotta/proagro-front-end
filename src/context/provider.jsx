fd46607d39b12dee76de14afdab9c9199c4df9bf
Lucas Lisboa <lucaslisboamotta@gmail.com>
2022-09-04 22:55:16 -0300

    feat: Criando estado do filtro

--- src/context/provider.jsx
+++ src/context/provider.jsx
@@ -2,10 +2,13 @@ import React, { useMemo, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import getAllExemptionRequests from '../api/getAllExemptionRequests';
 import context from './context';
+import { defaultFilter } from '../utils/filterFunctions';
 
 function Provider({ children }) {
   const [requestArray, setRequestArray] = useState([]);
   const [editRequest, setEditRequest] = useState({ edit: false, prevRequest: {} });
+  const [useFilter, setUseFilter] = useState(false);
+  const [filterParams, setFilterParams] = useState(defaultFilter);
 
   const updateRequestArray = async () => {
     const newRequestArray = await getAllExemptionRequests();
@@ -17,11 +20,19 @@ function Provider({ children }) {
     updateRequestArray,
     editRequest,
     setEditRequest,
+    useFilter,
+    setUseFilter,
+    filterParams,
+    setFilterParams,
   }), [
     requestArray,
     updateRequestArray,
     editRequest,
     setEditRequest,
+    useFilter,
+    setUseFilter,
+    filterParams,
+    setFilterParams,
   ]);
 
   useEffect(() => { updateRequestArray(); }, []);
